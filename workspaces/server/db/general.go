d0f776596e8907c06ea96e5d30d1dbccf17c172d
Pugma <132571355+Pugma@users.noreply.github.com>
2024-03-24 04:55:53 +0900

    これで初期化ができるようになったはず

--- workspaces/server/db/general.go
+++ workspaces/server/db/general.go
@@ -1,19 +1,57 @@
 package db
 
 import (
-	"database/sql"
+	"cmp"
 	"log/slog"
+	"net"
+	"os"
+	"time"
 
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/tanimutomo/sqlfile"
 )
 
-var db *sql.DB
+var db *sqlx.DB
 
-func SetUp() error {
-	_db, err := sql.Open("sqlite3", "../seeds/database.sqlite")
+func SetUpDB() error {
+	jst, err := time.LoadLocation("Asia/Tokyo")
 	if err != nil {
-		slog.Error("Failed connecting to db: %v", err)
+		return err
+	}
+	host := cmp.Or(os.Getenv("NS_MARIADB_HOSTNAME"), "localhost")
+	port := cmp.Or(os.Getenv("NS_MARIADB_PORT"), "3306")
+	conf := mysql.Config{
+		User:                 cmp.Or(os.Getenv("NS_MARIADB_USER"), "root"),
+		Passwd:               cmp.Or(os.Getenv("NS_MARIADB_PASSWORD"), "password"),
+		Net:                  "tcp",
+		Addr:                 net.JoinHostPort(host, port),
+		DBName:               cmp.Or(os.Getenv("NS_MARIADB_DATABASE"), "line_webhook"),
+		Loc:                  jst,
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	}
+	_db, err := sqlx.Connect("mysql", conf.FormatDSN())
+	if err != nil {
+		return err
+	}
+	if err := _db.Ping(); err != nil {
+		return err
 	}
 	db = _db
+	slog.Info("Connected to database")
+
+	// ここで初期値の代入をする
+	s := sqlfile.New()
+	err = s.File("../schema.sql")
+	if err != nil {
+		slog.Error("Failed to open sql file: %v", err)
+	}
+	_, err = s.Exec(db.DB)
+	if err != nil {
+		slog.Error("Failed to initialize: %v", err)
+	}
 	return nil
 }
