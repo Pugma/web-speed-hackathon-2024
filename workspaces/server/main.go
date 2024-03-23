b2c10f7f3b87b91af356fa6719d8ed6fbb805450
Pugma <132571355+Pugma@users.noreply.github.com>
2024-03-24 00:02:51 +0900

    echoでエンドポイントの設定忘れていたのを修正

--- workspaces/server/main.go
+++ workspaces/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"Pugma/webspeed2024/db"
+	"Pugma/webspeed2024/handler"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -13,6 +14,8 @@ func main(){
 
 	// DBのセットアップ
 	db.SetUp()
+
+	e.GET("/authors/:authorId", handler.GetAuthor)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
