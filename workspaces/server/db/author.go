a64fdf225cc313d2056e7723bac5151b1fa3cc88
Pugma <132571355+Pugma@users.noreply.github.com>
2024-03-23 23:45:59 +0900

    これで著者情報は取得できる？

--- workspaces/server/db/author.go
+++ workspaces/server/db/author.go
@@ -1,11 +1,21 @@
 package db
 
-import "log/slog"
+import (
+	"Pugma/webspeed2024/api"
+	"log/slog"
+)
 
-func SelectAuthor(authorId *string)  error {
-	_, err := db.Query("")
+func SelectAuthor(authorId *string) (api.GetAuthorDetail, error) {
+	row := db.QueryRow("SELECT * ", &authorId)
+	var authorDetail api.GetAuthorDetail
+	err := row.Scan(
+		&authorDetail.Books,
+		&authorDetail.Description,
+		&authorDetail.Id,
+		&authorDetail.Name,
+	)
 	if err != nil {
-		slog.Error("Failed to get author: %v", err)
+		slog.Error("Failed to convert type from db: %v", err)
 	}
-	return nil
+	return authorDetail, nil
 }
